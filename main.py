279b8fb11bd5d77539c7c88b775457e78aeda9c1
Diego <tucotony1396@gmail.com>
2020-04-01 20:19:46 -0500

    structure project

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import click
-from install_docker import install_docker
-from install_fast import install_all
-from utils import packages_gui, message_gui, message_click_colors, message_click
-from install_node import install_node, install_node_all
-from install_minikube import install_k8s_minikube
-
+# from install_docker import install_docker
+# from install_fast import install_all
+# from utils import packages_gui, message_gui, message_click_colors, message_click
+# from install_node import install_node, install_node_all
+# from install_minikube import install_k8s_minikube
+import platform
 
 @click.group()
 def cli():
     pass
 
 
-@cli.command(help='Install all apt packages')
-def all():
-    install_all()
+# @cli.command(help='Install all apt packages')
+# def all():
+#     install_all()
 
 
 # @cli.command(help='Install only apt sysadmin packages (htop,etc)')
@@ -26,31 +26,31 @@ def all():
 #     install_basic()
 
 
-@cli.command(help='Install docker ')
-def docker():
-    install_docker()
+# @cli.command(help='Install docker ')
+# def docker():
+#     install_docker()
 
 
-@cli.command(help='Install minikube ')
-def minikube():
-    install_k8s_minikube()
+# @cli.command(help='Install minikube ')
+# def minikube():
+#     install_k8s_minikube()
 
 
-@cli.command(help='Install NodeJs')
-@click.option('--version', default=10, help='Install nodejs 10')
-def nodejs(version):
-    install_node(version)
+# @cli.command(help='Install NodeJs')
+# @click.option('--version', default=10, help='Install nodejs 10')
+# def nodejs(version):
+#     install_node(version)
 
 
-@cli.command(help='Install NodeJs with packages')
-@click.option('--version', default=10, help='Install nodejs 10')
-def nodejs_all(version):
-    install_node_all(version)
+# @cli.command(help='Install NodeJs with packages')
+# @click.option('--version', default=10, help='Install nodejs 10')
+# def nodejs_all(version):
+#     install_node_all(version)
 
 
-@cli.command(help='Download important repositories')
-def github():
-    print("github")
+# @cli.command(help='Download important repositories')
+# def github():
+#     print("github")
 
 
 
