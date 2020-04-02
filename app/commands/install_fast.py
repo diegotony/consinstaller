13c4a6ad5508ef1bdecc327be411548029dab6b7
Diego <tucotony1396@gmail.com>
2020-04-01 23:07:12 -0500

    init files

--- app/commands/install_fast.py
+++ app/commands/install_fast.py
@@ -1,19 +1,4 @@
-import sys
-import os
-import click
-import yaml
-import emoji
-
-from commands import update_packages, install_packages_apt
-from utils import message_click_colors, init_cli
-
-with open('yaml/packages.yml') as f:
-    packages = yaml.load(f, Loader=yaml.FullLoader)
 
 
-def install_all():
-    update_packages()
-    install_packages_apt(
-        packages['packages_default'], "Default")
 
 
