ff95da605518ec7c0da5ce6036d74c09e210bf72
Diego <tucotony1396@gmail.com>
2020-04-03 20:03:12 -0500

    debian commands added

--- app/distro/debian/debian.py
+++ app/distro/debian/debian.py
@@ -1,17 +1,23 @@
 import click
-from app.commands.apt_commands import execute as apt_install_packages_default
+from app.commands.apt_commands import execute as apt_install
 from app.commands.install_docker import execute as docker_install
+from app.format.message_cli import init_cli as head
 
 @click.group()
 def cli():
     pass
 
 
-@cli.command(help='install packages default')
+@cli.command(help='install apt')
 def default():
-    # apt_install_packages_default()
-    docker_install()
+    head()
+    apt_install()
 
+
+@cli.command(help='install docker')
+def install_docker():
+    head()
+    docker_install()
 
 # def with_docker(help="install packages default and docker"):
 #     prepare_install()
